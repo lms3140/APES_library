237cef8d541c9ee706664d58355ee5b73e47ab06
Pru <korea314044@gmail.com>
2025-10-17 18:14:59 +0900

    feat: add Layout components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div style={{ width: 1200, margin: "0 auto" }}>
+      <h1>i'm Home</h1>
+    </div>
+  );
+}
---
+++ src/pages/Layout/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export function Footer() {
+  return (
+    <div className={styles.footerContainer}>
+      <h1>푸터입니다</h1>
+    </div>
+  );
+}
