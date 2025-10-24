a990ef2ccfbc3bb368dbe06921b354741e8c45da
shyun <exriik@gmail.com>
2025-10-24 18:16:36 +0900

    Header isLogin 수정

--- frontend/src/pages/Layout/Header/Header.jsx
+++ frontend/src/pages/Layout/Header/Header.jsx
@@ -1,37 +1,48 @@
 import style from "./Header.module.css";
+import { Link } from 'react-router-dom';
 import { MdArrowDropUp } from "react-icons/md";
+import { useSelector } from 'react-redux';
 
 
 export function HeaderLogin() {
+  const isLogin = useSelector((state) => state.auth.isLogin);
+
+
+
+  
   return (
     <div>
       <div className={style.userMenuContainer}>
         <div className={style.userMenuWrapper}>
           <ul>
-            <li>
-              <a href="#">로그아웃</a>
-            </li>
-            <li>
-              <button type="button">
-                <span>회원혜택 <MdArrowDropUp /></span>
-              </button>
-              <ul>
-                <li><a href="#">교보북클럽</a></li>
-                <li><a href="#">등급 혜택</a></li>
-                <li><a href="#">신규회원 혜택</a></li>
-                <li><a href="#">Prestige Lounge</a></li>
-                <li><a href="#">제휴 혜택</a></li>
-              </ul>
-            </li>
-            <li>
-              <a href="#">주문배송</a>
-            </li>
-            <li>
-              <a href="#">매장안내</a>
-            </li>
-            <li>
-              <a href="#">고객센터</a>
-            </li>
+            {/* 로그인 안했을 때 노출 */}
+            {!isLogin && 
+              <>
+                <li><Link to="/">회원가입</Link></li>
+                <li><Link to="/">로그인</Link></li>
+              </>
+            }
+            {/* 로그인 했을 때 노출 */}
+            {isLogin &&
+              <>
+                <li><Link to="/">로그아웃</Link></li>
+                <li>
+                  <button type="button">
+                    <span>회원혜택 <MdArrowDropUp /></span>
+                  </button>
+                  <ul>
+                    <li><Link to="/">교보북클럽</Link></li>
+                    <li><Link to="/">등급 혜택</Link></li>
+                    <li><Link to="/">신규회원 혜택</Link></li>
+                    <li><Link to="/">Prestige Lounge</Link></li>
+                    <li><Link to="/">제휴 혜택</Link></li>
+                  </ul>
+                </li>
+              </>
+            }
+            <li><Link to="/">주문배송</Link></li>
+            <li><Link to="/">매장안내</Link></li>
+            <li><Link to="/">고객센터</Link></li>
           </ul>
         </div>
       </div>
--- frontend/src/pages/Mypage/Mypage.jsx
+++ frontend/src/pages/Mypage/Mypage.jsx
@@ -1,9 +1,27 @@
 import { HeaderLogin } from "../Layout/Header/HeaderLogin";
 
 export function Mypage() {
+
+
+
+
     return (
-        <div className={styles.layout}>
-            <HeaderLogin />
-        </div>
+            <main className="">
+                <section>
+                    <div className="">
+                        <ul className="">
+                            <li className="">
+                                <a href="https://www.kyobobook.co.kr" className="">HOME</a>
+                            </li>
+                            <li>
+                                <a href="/main" className="" disabled="disabled">마이</a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+
+
+            </main>
+
     );
 }
