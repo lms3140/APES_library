e0f712346d8e167cd9f8fadfccdd85caa1b22bcd
Seongeunj <seongeunj360@gmail.com>
2025-10-29 18:20:41 +0900

    feat: 로그인페이지, 회원가입 인트로페이지 구현

--- frontend/src/pages/Layout/SignupIntro.jsx
+++ frontend/src/pages/Layout/SignupIntro.jsx
@@ -1,57 +1,49 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
-import {FaGoogle, FaMobileAlt, FaEnvelope} from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { FaGoogle, FaMobileAlt, FaEnvelope } from "react-icons/fa";
 import styles from "./SignupIntro.module.css";
 
 const SignupIntro = () => {
     const navigate = useNavigate();
 
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.container}>
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                <img src="#" alt="교보문고 로고" />
+            </div>
 
-                {/* 로고 */}
-                <div className={styles.SignupLogo}>
-                    <img src="#" alt="교보문고 로고"/>
-                </div>
+            <button className={styles.btnSignup}>회원가입</button>
 
-                {/* 회원가입 버튼 */}
-                <button className={styles.btnSignup}>회원가입</button>
+            <div className={styles.signupLinks}>
+                <a href="#">법인회원 가입</a>
+                <span>|</span>
+                <a href="#">로그인</a>
+            </div>
 
-                {/* 하단 링크 */}
-                <div className={styles.SignupLinks}>
-                    <a href="#">법인회원 가입</a>
-                    <span>|</span>
-                    <a href="#">로그인</a>
-                </div>
+            <div className={styles.socialSignup}>
+                <button type="button" className={styles.btnKakao}>
+                    카카오 회원가입
+                </button>
+                <button type="button" className={styles.btnNaver}>
+                    네이버 회원가입
+                </button>
+            </div>
 
-                {/* SNS 회원가입 버튼 */}
-                <div className={styles.socialSignup}>
-                    <button type="button" className={styles.btnKakao}>
-                        카카오 회원가입
-                    </button>
-                    <button type="button" className={styles.btnNaver}>
-                        네이버 회원가입
-                    </button>
-                </div>
+            <p className={styles.otherTitle}>다른 방법으로 회원가입</p>
 
-                {/* 다른 방법 */}
-                <p className={styles.otherTitle}>다른 방법으로 회원가입</p>
-                <div className={styles.otherMethods}>
-                    <button className={styles.iconBtn}>
-                        <div className={styles.icon}><FaGoogle/></div>
-                        <span className={styles.iconLabel}>구글</span>
-                    </button>
-                    <button className={styles.iconBtn}>
-                        <div className={styles.icon}><FaMobileAlt/></div>
-                        <span className={styles.iconLabel}>휴대폰</span>
-                    </button>
-                    <button className={styles.iconBtn}>
-                        <div className={styles.icon}><FaEnvelope/></div>
-                        <span className={styles.iconLabel}>이메일</span>
-                    </button>
-                </div>
-
+            <div className={styles.otherMethods}>
+                <button className={styles.iconBtn}>
+                    <div className={styles.icon}><FaGoogle /></div>
+                    <span className={styles.iconLabel}>구글</span>
+                </button>
+                <button className={styles.iconBtn}>
+                    <div className={styles.icon}><FaMobileAlt /></div>
+                    <span className={styles.iconLabel}>휴대폰</span>
+                </button>
+                <button className={styles.iconBtn}>
+                    <div className={styles.icon}><FaEnvelope /></div>
+                    <span className={styles.iconLabel}>이메일</span>
+                </button>
             </div>
         </div>
     );
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./pages/Layout/Layout";
 import { Home } from "./pages/Home/Home";
 import { StoreInfo } from "./pages/storeInfo/StoreInfo";
-import Login from "./pages/Layout/Login.jsx"
-import SignupIntro from "./pages/Layout/SignupIntro.jsx"
 
 export const router = createBrowserRouter([
   {
