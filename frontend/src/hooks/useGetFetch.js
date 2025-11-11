35ee057eb1bb54e45184caa9834b6bb742a1f511
Pru <korea314044@gmail.com>
2025-11-12 02:29:57 +0900

    refactor: Home 페이지 구조 정리 및 데이터 요청 훅 분리

---
+++ frontend/src/hooks/useGetFetch.js
@@ -0,0 +1,49 @@
+/**
+ * [Get] 데이터 요청 훅
+ * @param {string} url get요청을 보낼 URL
+ * @returns {{ data: any | null, isError: boolean, isLoading: boolean }}
+ *
+ *
+ * @example
+ * ```
+ *  // 사용예시 코드!!
+ *
+ *  const url = "http://example.com/getSomething"
+ *  const {data, isLoading, isError} = useGetFetch(url);
+ *
+ *  if(isError){
+ *    return <h1>에러발생!</h1>
+ *  }
+ *
+ *  return
+ *    <div>
+ *      {isLoading ? <h1>로딩중</h1> : <h1>로딩완료</h1>}
+ *    </div>
+ *
+ * ```
+ *
+ */
+export const useGetFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getFetch() {
+      setIsLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (e) {
+        console.error(e);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getFetch();
+  }, []);
+
+  return { isError, isLoading, data };
+};
