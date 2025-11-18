328d105a46bcb58c1c9b87830e712fda850aaf12
kim <a01064149895@gmail.com>
2025-11-18 18:24:21 +0900

    style: Search 관련 css 적용, 수정중

--- frontend/src/components/Search/SearchFilter.jsx
+++ frontend/src/components/Search/SearchFilter.jsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react";
 import styles from "../../pages/Search/Search.module.css";
 import { TfiReload } from "react-icons/tfi";
+import { FaPlus } from "react-icons/fa6";
+import { FaMinus } from "react-icons/fa6";
 
 export function SearchFilter() {
   const [isSearchOpen, setIsSearchOpen] = useState(true);
@@ -41,26 +43,30 @@ export function SearchFilter() {
       <form ref={formRef}>
         <div className={styles.filterBox}>
           <div className={styles.filterOptionGroup}>
-            <p className={styles.filterSmallTitle}>검색조건</p>
             <button
               type="button"
+              className={styles.filterOptionBtn}
               onClick={() => setIsSearchOpen(!isSearchOpen)}
             >
-              {isSearchOpen ? "-" : "+"}
+              <span className={styles.filterSmallTitle}>검색조건</span>
+              {isSearchOpen ? <FaMinus /> : <FaPlus />}
             </button>
           </div>
           {isSearchOpen && (
             <div>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 상품명
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 저자/역자
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 출판사
               </label>
             </div>
@@ -68,61 +74,77 @@ export function SearchFilter() {
         </div>
         <div className={styles.filterBox}>
           <div className={styles.filterOptionGroup}>
-            <p className={styles.filterSmallTitle}>학습서필터</p>
-            <button type="button" onClick={() => setIsStudyOpen(!isStudyOpen)}>
-              {isStudyOpen ? "-" : "+"}
+            <button
+              type="button"
+              className={styles.filterOptionBtn}
+              onClick={() => setIsStudyOpen(!isStudyOpen)}
+            >
+              <span className={styles.filterSmallTitle}>학습서필터</span>
+              {isStudyOpen ? <FaMinus /> : <FaPlus />}
             </button>
           </div>
           {isStudyOpen && (
             <div>
-              <p>학년</p>
+              <p className={styles.subTitle}>학년</p>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 초등학교 1학년
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 초등학교 2학년
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 초등학교 3학년
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 초등학교 4학년
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 초등학교 5학년
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 초등학교 6학년
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 예비 중학생
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 고등학교 공통
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 고등학교 1학년
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 고등학교 2학년
               </label>
-              <p>학기</p>
+              <p className={styles.subTitle}>학기</p>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 공통
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 1학기
               </label>
             </div>
@@ -131,62 +153,75 @@ export function SearchFilter() {
 
         <div className={styles.filterBox}>
           <div className={styles.filterOptionGroup}>
-            <p className={styles.filterSmallTitle}>혜택/조건</p>
             <button
               type="button"
+              className={styles.filterOptionBtn}
               onClick={() => setIsBenefitOpen(!isBenefitOpen)}
             >
-              {isBenefitOpen ? "-" : "+"}
+              <span className={styles.filterSmallTitle}>혜택/조건</span>
+              {isBenefitOpen ? <FaMinus /> : <FaPlus />}
             </button>
           </div>
           {isBenefitOpen && (
             <div>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 쿠폰
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 이벤트
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 무료배송
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 핫트랙스배송
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 19세상품만보기
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 19세상품제외
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 POD
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 분철상품
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 판매마감제외
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 품절판제외
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 패키지
               </label>
               <label className={styles.filterCheckbox}>
-                <input type="checkbox" />
+                <input type="checkbox" className={styles.checkboxInput} />
+                <span className={styles.checkboxCustom}></span>
                 개정판
               </label>
             </div>
