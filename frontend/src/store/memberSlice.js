0f49ad6bb4455d24d7ec3c55cc9d5ad1fc1c94cc
Pru <korea314044@gmail.com>
2025-11-24 15:39:55 +0900

    feat: member slice 구현 및 미사용 주석 제거

--- frontend/src/store/memberSlice.js
+++ frontend/src/store/memberSlice.js
@@ -3,17 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 export const memberSlice = createSlice({
   name: "member",
   initialState: {
-    userId: "",
-    name: "",
+    userId: null,
   },
   reducers: {
     setUserId: (state, action) => {
-      state.userId = action.payload?.userId;
+      state.userId = action.payload;
     },
   },
 });
 
-// Action creators are generated for each case reducer function
-export const { incrementByAmount } = memberSlice.actions;
+export const { setUserId } = memberSlice.actions;
 
 export default memberSlice.reducer;
