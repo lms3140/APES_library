380b2fcfead0e076445a96f0614f672379e674cd
Pru <korea314044@gmail.com>
2025-11-28 02:08:31 +0900

    feat: kakaoPay 백엔드 구현

--- backend/src/main/java/com/bookshop/entity/PurchaseOrder.java
+++ backend/src/main/java/com/bookshop/entity/PurchaseOrder.java
@@ -3,21 +3,67 @@ package com.bookshop.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class PurchaseOrder extends BaseCreatedEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
 
-    private String orderStatus;
-    private Integer totalPrice;
+    private String orderStatus = "READY";
+
+    @Setter
+    private Integer originalAmount;
+
+    @Setter
+    private Integer totalAmount;
+
+    @Setter
+    private int earnedPoint;
+
+    @Setter
+    private String tid;
+
+    private LocalDateTime paidAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Address address;
+
+    // 편의 생성자
+    public PurchaseOrder(Member member, Address address, Integer totalAmount,int originalAmount) {
+        this.member = member;
+        this.address = address;
+        this.totalAmount = totalAmount;
+        this.orderStatus = "READY";
+        this.originalAmount = originalAmount;
+    }
+
+    // 결제 승인 시
+    public void approve() {
+        this.orderStatus = "PAID";
+        this.paidAt = LocalDateTime.now();
+    }
+
+    public void cancel() {
+        this.orderStatus = "CANCEL";
+    }
+
+    public void fail() {
+        this.orderStatus = "FAIL";
+    }
+
+    public void error(){
+        this.orderStatus = "ERROR";
+    }
+
+
 }
---
+++ backend/src/main/java/com/bookshop/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.bookshop.repository;
+
+import com.bookshop.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
